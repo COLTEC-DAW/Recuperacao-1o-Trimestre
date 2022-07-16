05e3133eb4c2ef791b2c79681da59a2e6d83cbe2
Gabriel Rodrigues Pereira <gabrielrpereira.dev@gmail.com>
2022-07-16 08:22:53 -0300

    Finalização

---
+++ table.js
@@ -0,0 +1,277 @@
+document.write('\
+<table class="table table-borderless table-striped">\
+    <thead>\
+      <tr>\
+        <th scope="col">Nº</th>\
+        <th scope="col">Nome</th>\
+        <th scope="col">Partipação</th>\
+        <th scope="col">Ano</th>\
+      </tr>\
+    </thead>\
+    <tbody>\
+      <tr>\
+        <th scope="row">1</th>\
+        <td>Futureworld</td>\
+        <td>Producer: Animated Face and Animated Hand Film</td>\
+        <td>1976</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">2</th>\
+        <td>Star Trek II: The Wrath of Khan</td>\
+        <td>Computer Graphics: Industrial Light & Magic</td>\
+        <td>1982</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">3</th>\
+        <td>Toy Story</td>\
+        <td>Executive Producer, Renderman(R) Software Development</td>\
+        <td>1995</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">4</th>\
+        <td>A Bug\'s Life</td>\
+        <td>Executive Team - uncredited</td>\
+        <td>1998</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">5</th>\
+        <td>Toy Story 2</td>\
+        <td>Executive Team - uncredited</td>\
+        <td>1999</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">6</th>\
+        <td>Monsters, Inc.</td>\
+        <td>Executive Team - uncredited</td>\
+        <td>2001</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">7</th>\
+        <td>Finding Nemo</td>\
+        <td>Executive Team - uncredited</td>\
+        <td>2003</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">8</th>\
+        <td>The Incredibles</td>\
+        <td>Executive Team - uncredited</td>\
+        <td>2004</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">8</th>\
+        <td>Cars</td>\
+        <td>Executive team</td>\
+        <td>2006</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">9</th>\
+        <td>Meet the Robinsons</td>\
+        <td>Executive team</td>\
+        <td>2007</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">10</th>\
+        <td>Ratatouille</td>\
+        <td>Executive team</td>\
+        <td>2008</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">11</th>\
+        <td>WALL-E</td>\
+        <td>Pixar Senior Staff</td>\
+        <td>2008</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">12</th>\
+        <td>Tinker Bell</td>\
+        <td>Executive Team: Pixar and Walt Disney Animation Studios</td>\
+        <td>2008</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">13</th>\
+        <td>Bolt</td>\
+        <td>Executive Team</td>\
+        <td>2008</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">14</th>\
+        <td>Up</td>\
+        <td>Pixar Senior Staff</td>\
+        <td>2009</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">15</th>\
+        <td>Tinker Bell and the Lost Treasure</td>\
+        <td>Executive Team: Pixar and Walt Disney Animation Studios</td>\
+        <td>2009</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">16</th>\
+        <td>The Princess and the Frog</td>\
+        <td>Disney Senior Staff</td>\
+        <td>2009</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">17</th>\
+        <td>Toy Story 3</td>\
+        <td>Pixar Executive Team</td>\
+        <td>2010</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">18</th>\
+        <td>Tinker Bell and the Great Fairy Rescue</td>\
+        <td>Executive Team: Pixar and Walt Disney Animation Studios</td>\
+        <td>2010</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">19</th>\
+        <td>Tangled</td>\
+        <td>Studio Leadership</td>\
+        <td>2010</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">20</th>\
+        <td>Cars 2</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2011</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">21</th>\
+        <td>Winnie the Pooh</td>\
+        <td>Studio Leadership</td>\
+        <td>2011</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">22</th>\
+        <td>Brave</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2012</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">23</th>\
+        <td>Secret of the Wings</td>\
+        <td>Executive Team: Pixar and Walt Disney Animation Studios</td>\
+        <td>2012</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">24</th>\
+        <td>Wreck-It Ralph</td>\
+        <td>Studio Leadership</td>\
+        <td>2012</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">25</th>\
+        <td>Monsters University</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2013</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">26</th>\
+        <td>Planes</td>\
+        <td>Studio Leadership: Walt Disney Animation Studios</td>\
+        <td>2013</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">27</th>\
+        <td>Frozen</td>\
+        <td>Studio Leadership</td>\
+        <td>2013</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">28</th>\
+        <td>The Pirate Fairy</td>\
+        <td>Studio Leadership: Walt Disney Animation Studios</td>\
+        <td>2014</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">29</th>\
+        <td>Planes: Fire & Rescue</td>\
+        <td>Studio Leadership: Walt Disney Animation Studios</td>\
+        <td>2014</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">30</th>\
+        <td>Big Hero 6</td>\
+        <td>Studio Leadership</td>\
+        <td>2014</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">31</th>\
+        <td>Tinker Bell and the Legend of the NeverBeast</td>\
+        <td>Studio Leadership: Walt Disney Animation Studios</td>\
+        <td>2014</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">32</th>\
+        <td>Inside Out</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2015</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">33</th>\
+        <td>The Good Dinosaur</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2015</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">34</th>\
+        <td>Zootopia</td>\
+        <td>Studio Leadership</td>\
+        <td>2016</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">35</th>\
+        <td>Finding Dory</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2016</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">36</th>\
+        <td>Moana</td>\
+        <td>Studio Leadership</td>\
+        <td>2016</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">37</th>\
+        <td>Cars 3</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2017</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">38</th>\
+        <td>Coco</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2017</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">39</th>\
+        <td>Incredibles 2</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2018</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">40</th>\
+        <td>Ralph Breaks the Internet</td>\
+        <td>Studio Leadership</td>\
+        <td>2018</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">41</th>\
+        <td>Toy Story 4</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2019</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">42</th>\
+        <td>Frozen II</td>\
+        <td>Studio Leadership</td>\
+        <td>2019</td>\
+      </tr>\
+      <tr>\
+        <th scope="row">43</th>\
+        <td>Onward</td>\
+        <td>Pixar Senior Leadership Team</td>\
+        <td>2020</td>\
+      </tr>\
+    </tbody>\
+</table>');
